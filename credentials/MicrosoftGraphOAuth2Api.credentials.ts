5de59f128f0504e7a24dd320c59e1610e9cf3c28
Juan Pujol <30832+juanpujol@users.noreply.github.com>
2026-02-04 13:37:31 -0300

    refactor(credentials): simplify OAuth2 by extending microsoftOAuth2Api
    - Extend n8n's built-in microsoftOAuth2Api instead of generic oAuth2Api
    - Add read-only scopes (Sites.Read.All, Files.Read.All)
    - Add credential test endpoint for connection validation

--- credentials/MicrosoftGraphOAuth2Api.credentials.ts
+++ credentials/MicrosoftGraphOAuth2Api.credentials.ts
@@ -1,52 +1,36 @@
-import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+import type {
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+	Icon,
+} from 'n8n-workflow';
 
 export class MicrosoftGraphOAuth2Api implements ICredentialType {
 	name = 'microsoftGraphOAuth2Api';
 
-	extends = ['oAuth2Api'];
+	extends = ['microsoftOAuth2Api'];
 
 	displayName = 'Microsoft Graph OAuth2 API';
 
-	documentationUrl = 'https://docs.microsoft.com/en-us/graph/auth-v2-user';
+	documentationUrl =
+		'https://learn.microsoft.com/en-us/graph/auth-v2-user';
 
-	icon = 'file:icons/Microsoft.svg' as const;
+	icon: Icon = 'file:icons/Microsoft.svg';
 
 	properties: INodeProperties[] = [
-		{
-			displayName: 'Grant Type',
-			name: 'grantType',
-			type: 'hidden',
-			default: 'authorizationCode',
-		},
-		{
-			displayName: 'Authorization URL',
-			name: 'authUrl',
-			type: 'hidden',
-			default: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-		},
-		{
-			displayName: 'Access Token URL',
-			name: 'accessTokenUrl',
-			type: 'hidden',
-			default: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-		},
 		{
 			displayName: 'Scope',
 			name: 'scope',
 			type: 'hidden',
-			default: 'openid offline_access Sites.ReadWrite.All Files.ReadWrite.All',
-		},
-		{
-			displayName: 'Auth URI Query Parameters',
-			name: 'authQueryParameters',
-			type: 'hidden',
-			default: 'response_mode=query',
-		},
-		{
-			displayName: 'Authentication',
-			name: 'authentication',
-			type: 'hidden',
-			default: 'body',
+			default:
+				'openid offline_access Sites.Read.All Sites.ReadWrite.All Files.Read.All Files.ReadWrite.All',
 		},
 	];
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://graph.microsoft.com/v1.0',
+			url: '/sites?search=*&$top=1',
+		},
+	};
 }
